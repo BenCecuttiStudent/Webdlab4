1c28b9ea9968daeafb35b2470b76a972d7c3cb90
Benjamin Cecutti <benjamin.cecutti@dcmail.ca>
2023-12-05 23:22:32 -0500

    Initial Commit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+# email:string
+# password_digets:string
+# name:string
+#
+# password:string virtual
+# password_confirmation:string virutal
+
+class User < ApplicationRecord
+    has_secure_password
+
+    validates :email, presence: true, format: { with: /\A[_a-z0-9-]+(\.[_a-z0-9-]+)*(\+[a-z0-9-]+)?@[a-z0-9-]+(\.[a-z0-9-]+)*\Z/, message: "must be a valid email address" }
+    validates :password_digest, presence: true
+end
---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+Rails.application.routes.draw do
+  
+
+  # GET /about
+  get "about-us", to: "about#index", as: :about
+    
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
+  # Defines the root path route ("/")
+  root to: "main#index"
+  
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+end
