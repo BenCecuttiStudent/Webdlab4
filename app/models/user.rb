eff384c08267cc477e0ec61c96af2673a053286c
Benjamin Cecutti <benjamin.cecutti@dcmail.ca>
2023-12-06 02:02:11 -0500

    login functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,4 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :email, presence: true, format: { with: /\A[_a-z0-9-]+(\.[_a-z0-9-]+)*(\+[a-z0-9-]+)?@[a-z0-9-]+(\.[a-z0-9-]+)*\Z/, message: "must be a valid email address" }
-    validates :password_digest, presence: true
 end
