f51d3f82427fee5d7902ea8b825c6bd6f4656a7c
Benjamin Cecutti <benjamin.cecutti@dcmail.ca>
2023-12-06 03:13:36 -0500

    Reset Password Form

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
 
   delete "logout", to: "sessions#destroy"
 
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+
   # Defines the root path route ("/")
   root to: "main#index"
   
